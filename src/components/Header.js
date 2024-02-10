03ef0a6182682c2c48ca7404d76de3e641c8094b
Anna Kioko <annakioko00@gmail.com>
2024-02-10 22:16:10 +0300

    completed assignment

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import react from "react";
+import blogData from "../data/blog";
+
+export default function Header({ name }) {
+    return (
+      <header>
+        <h1>{name}</h1>
+      </header>
+    );
+}
